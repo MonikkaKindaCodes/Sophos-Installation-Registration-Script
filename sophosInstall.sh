abec206db97c7b31463a309ce4a1b7e0ef178c3b
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-02-02 13:17:47 -0600

    Sophos Install Script

---
+++ sophosInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir /private/var/tmp/sophos
+cd /private/var/tmp/sophos
+
+#Removing Sophos
+defaults write /Library/Preferences/com.sophos.sav TamperProtectionEnabled -int 0
+killall -kill SophosServiceManager
+/Library/Application\ Support/Sophos/saas/Installer.app/Contents/MacOS/tools/InstallationDeployer --remove
+
+# Installing Sophos
+curl -L -O "https://api-cloudstation-us-east-2.prod.hydra.sophos.com/api/download/c31b008ae3658cf18769e5f5eae213c7/SophosInstall.zip"
+unzip SophosInstall.zip
+chmod a+x /private/var/tmp/sophos/Sophos\ Installer.app/Contents/MacOS/Sophos\ Installer
+chmod a+x /private/var/tmp/sophos/Sophos\ Installer.app/Contents/MacOS/tools/com.sophos.bootstrap.helper
+sudo /private/var/tmp/sophos/Sophos\ Installer.app/Contents/MacOS/Sophos\ Installer --install;
+/bin/rm -rf /private/var/tmp/sophos;
+
+#Registering Sophos: REBOOT REQUIRED POST INSTALL
+sudo killall SophosConfigD 
+sudo launchctl stop com.sophos.mcs 
+
+exit 0      ## Success
+exit 1      ## Failure
